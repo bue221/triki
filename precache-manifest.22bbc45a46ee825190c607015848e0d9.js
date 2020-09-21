02be31cb2dcdb6d13401ff34db16c8fcd7ff127f
bue221 <camiloplaza3@gmail.com>
2020-09-21 06:38:55 -0500

    Updates

---
+++ precache-manifest.22bbc45a46ee825190c607015848e0d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c7301f5e58f5880d6fb195fdb98589",
+    "url": "/triki/index.html"
+  },
+  {
+    "revision": "513320c4c84d432cb502",
+    "url": "/triki/static/css/main.ddd6cc72.chunk.css"
+  },
+  {
+    "revision": "66d08d37553bff5aec23",
+    "url": "/triki/static/js/2.bfc6c8aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triki/static/js/2.bfc6c8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513320c4c84d432cb502",
+    "url": "/triki/static/js/main.452efb5f.chunk.js"
+  },
+  {
+    "revision": "ff9883c5d1c9b051d1bb",
+    "url": "/triki/static/js/runtime-main.65bd5159.js"
+  }
+]);
